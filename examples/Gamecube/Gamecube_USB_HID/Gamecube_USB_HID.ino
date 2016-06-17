6b5fca854e5e8b5e4156e61495155ed04d45b8ec
NicoHood <NicoHood@users.noreply.github.com>
2016-06-17 22:53:41 +0200

    Updated USB HID example

--- examples/Gamecube/Gamecube_USB_HID/Gamecube_USB_HID.ino
+++ examples/Gamecube/Gamecube_USB_HID/Gamecube_USB_HID.ino
@@ -1,139 +1,61 @@
 /*
- Copyright (c) 2014-2015 NicoHood
- See the readme for credit to other people.
+  Copyright (c) 2014-2016 NicoHood
+  See the readme for credit to other people.
 
- Gamecube_USB_HID example
- Use your Gamecube controller with PC as generic HID interface.
- (Optional) Serial debug output is displayed
- (Optional) Press A on the controller to enable rumble.
- The led indicated errors.
+  Gamecube_USB_HID example
+  Use your Gamecube controller with PC as generic HID interface.
+  The led indicated errors.
 
- This needs the HID Project.
- https://github.com/NicoHood/HID
+  This needs the HID Project.
+  https://github.com/NicoHood/HID
 */
 
+#include "HID-Project.h"
 #include "Nintendo.h"
 
-// do you want to use Serial debug output?
-#define DEBUG
-// rumble if button A is pressed?
-//#define RUMBLE
+// Define a Gamecube Controller
+CGamecubeController GamecubeController1(7);
 
-// pin definitions
-#define pinGamecubeController1 2
+// Pin definitions
 #define pinLed LED_BUILTIN
 
-// variables to save controller information
-Gamecube_Data_t GamecubeData;
-#ifdef DEBUG
-Gamecube_Status_t GamecubeStatus;
-#endif
-
-void setup() {
-#ifdef DEBUG
-  // start debug serial
-  Serial.begin(115200);
-  Serial.println(F("Starting controller emulation"));
-  Serial.println();
-#endif
-
-  // set up debug led
+void setup()
+{
+  // Set up debug led
   pinMode(pinLed, OUTPUT);
 
-  // sends a clean report to the host. This is important on any Arduino type.
+  // Sends a clean report to the host. This is important on any Arduino type.
   Gamepad.begin();
 }
 
-void loop() {
-#ifdef RUMBLE
-  static bool rumble = false;
-#endif
-  static bool error = true;
 
-  if (error) {
-    // Initialize the gamecube controller by sending it a null byte.
-    // This is unnecessary for a standard controller, but is required for the
-    // Wavebird.
-#ifdef DEBUG
-    if (initGamecubeController(pinGamecubeController1, GamecubeStatus))
-#else
-    if (Gamecube.begin(pinGamecubeController1))
-#endif
-      error = false;
-    else
-      error = true;
-  }
-  // controller is initialized successfull
-  else {
-    // try to read data from the controller, translate and send to USB device
-#ifdef RUMBLE
-    if (Gamecube.read(pinGamecubeController1, GamecubeData, rumble)) {
-      // enable rumble next time if button A is pressed and send to USB
-      rumble = GamecubeData.a;
-      sendGamecubeReport(GamecubeData);
-    }
-#else
-    if (Gamecube.read(pinGamecubeController1, GamecubeData))
-      sendGamecubeReport(GamecubeData);
-#endif
-    else {
-#ifdef DEBUG
-      Serial.println(F("Could not connect to the controller."));
-      Serial.println();
-#endif
-      error = true;
-    }
-  }
+void loop()
+{
+  // Try to read the controller data
+  if (GamecubeController1.read())
+  {
+    // Send controller data to the USB interface
+    auto report = GamecubeController1.getReport();
+    sendGamecubeReport(report);
 
-  // led indicates error state
-  digitalWrite(pinLed, error);
-
-  // retry every second, else delay a bit to not overload PC
-  if (error) {
-    // reset USB controller report
-    Gamepad.end();
-    delay(1000);
+    // delay a bit to not overload PC
+    delay(10);
   }
   else
-    delay(10);
+  {
+    // Center gamepad on disconnect
+    Gamepad.end();
+
+    // Add debounce if reading failed
+    digitalWrite(pinLed, HIGH);
+    delay(1000);
+  }
+  digitalWrite(pinLed, LOW);
 }
 
-bool initGamecubeController(uint8_t pin, Gamecube_Status_t &gc_status) {
-  // try to initialize controller
-  if (Gamecube.begin(pin, gc_status)) {
-    // print the values for debug
-    Serial.println(F("Controller initialization successfull."));
 
-    // print device information
-    Serial.print(F("Device: "));
-    switch (gc_status.device) {
-      case NINTENDO_DEVICE_GC_WIRED:
-        Serial.println(F("Original Nintendo Gamecube Controller"));
-        break;
-      default:
-        Serial.print(F("Unknown "));
-        Serial.println(gc_status.device, HEX);
-        break;
-    }
-
-    // print rumble state
-    Serial.print(F("Rumble "));
-    if (gc_status.rumble)
-      Serial.println(F("on"));
-    else
-      Serial.println(F("off"));
-
-    Serial.println();
-    return true;
-  }
-  else {
-    Serial.println(F("Controller initialization failed."));
-    Serial.println();
-    return false;
-  }
-}
-
-void sendGamecubeReport(Gamecube_Data_t &gc_report) {
+void sendGamecubeReport(Gamecube_Report_t &gc_report)
+{
   // The two control sticks
   Gamepad.xAxis((gc_report.xAxis - 0x80) << 8);
   Gamepad.yAxis(~(gc_report.yAxis - 0x80) << 8); // y stick needs to be inverted
@@ -149,44 +71,37 @@ void sendGamecubeReport(Gamecube_Data_t &gc_report) {
   Gamepad.rzAxis(gc_report.right - 0x80);
 
   // D-Pad:
-  switch (gc_report.dpad) {
-    // linear
-    case NINTENDO_GAMECUBE_DPAD_LEFT:
-      Gamepad.dPad1(GAMEPAD_DPAD_LEFT);
-      break;
-    case NINTENDO_GAMECUBE_DPAD_RIGHT:
-      Gamepad.dPad1(GAMEPAD_DPAD_RIGHT);
-      break;
-    case NINTENDO_GAMECUBE_DPAD_DOWN:
-      Gamepad.dPad1(GAMEPAD_DPAD_DOWN);
-      break;
-    case NINTENDO_GAMECUBE_DPAD_UP:
-      Gamepad.dPad1(GAMEPAD_DPAD_UP);
-      break;
-    // diagnoal
-    case NINTENDO_GAMECUBE_DPAD_DOWN_LEFT:
-      Gamepad.dPad1(GAMEPAD_DPAD_DOWN_LEFT);
-      break;
-    case NINTENDO_GAMECUBE_DPAD_DOWN_RIGHT:
-      Gamepad.dPad1(GAMEPAD_DPAD_DOWN_RIGHT);
-      break;
-    case NINTENDO_GAMECUBE_DPAD_UP_LEFT:
-      Gamepad.dPad1(GAMEPAD_DPAD_UP_LEFT);
-      break;
-    case NINTENDO_GAMECUBE_DPAD_UP_RIGHT:
-      Gamepad.dPad1(GAMEPAD_DPAD_UP_RIGHT);
-      break;
-    // centered
-    default:
-      Gamepad.dPad1(GAMEPAD_DPAD_CENTERED);
-      break;
-  } // end switch
+  if (gc_report.dup && gc_report.dright) {
+    Gamepad.dPad1(GAMEPAD_DPAD_UP_RIGHT);
+  }
+  else if (gc_report.dup && gc_report.dleft) {
+    Gamepad.dPad1(GAMEPAD_DPAD_UP_LEFT);
+  }
+  else if (gc_report.ddown && gc_report.dright) {
+    Gamepad.dPad1(GAMEPAD_DPAD_DOWN_RIGHT);
+  }
+  else if (gc_report.ddown && gc_report.dleft) {
+    Gamepad.dPad1(GAMEPAD_DPAD_DOWN_LEFT);
+  }
+  else if (gc_report.dup) {
+    Gamepad.dPad1(GAMEPAD_DPAD_UP);
+  }
+  else if (gc_report.ddown) {
+    Gamepad.dPad1(GAMEPAD_DPAD_DOWN);
+  }
+  else if (gc_report.dright) {
+    Gamepad.dPad1(GAMEPAD_DPAD_RIGHT);
+  }
+  else if (gc_report.dleft) {
+    Gamepad.dPad1(GAMEPAD_DPAD_LEFT);
+  }
 
   // 8 Gamecube buttons
   Gamepad.buttons(0x00UL | (gc_report.buttons0 & 0x1F) | ((gc_report.buttons1 & 0x70) << 1));
 
   // You could add a zero value correction here or a minimum
-  // for the left/right shoulder triggers but Windows can calibrate controllers very well.
+  // for the left/right shoulder triggers but the os can
+  // (and should) calibrate controllers very well.
 
   // Write the information to the PC
   Gamepad.write();
